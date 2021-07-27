54b198015c2fa6984ecb6dbed3a4bffbfa138d3c
Earthman12 <dteran@lpl.arizona.edu>
2021-07-27 11:33:24 -0700

    Creating GUI using PyQt5 for fourier transforms

--- FourierTransformV5.py
+++ FourierTransformV5.py
@@ -5,7 +5,8 @@ Created on Wed Jul 21 09:52:55 2021
 @author: Earthman
 """
 
-import PyQt5
+
+import PyQt5 as qt
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.ndimage as ndi
@@ -16,3 +17,32 @@ from tkinter.filedialog import askopenfilename
 from tkinter import Tk
 from astroscrappy import detect_cosmics
 
+#   GUI OBJECT
+class transform_gui(object):
+    
+    def __init__(self):
+        #   VARIABLES
+        
+        #   CREATE WIDGETS
+        #   Every GUI must have one instance of QApplication(), inside the brackets[] would be parameters passed to the application
+        self.app = qt.QtWidgets.QApplication([])
+        self.app.setStyle('Fusion')
+
+        self.label = qt.QtWidgets.QLabel("Whaddup playa")
+        self.label.show()
+
+def main():
+    
+    # GUI object
+    gui = transform_gui()
+    
+    #   This hands control over to Qt and will run the application till the user closes it
+    gui.app.exec()
+
+    print ("End of program")
+    
+#DRIVER CODE
+
+if __name__ == '__main__' :
+
+    main()
