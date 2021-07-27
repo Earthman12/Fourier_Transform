f967779689f667dfb5c0adec22a7bb0cb726a035
Earthman12 <dteran@lpl.arizona.edu>
2021-07-27 11:49:40 -0700

    Adding image functionality, need to figure out how to display it

--- FourierTransformV5.py
+++ FourierTransformV5.py
@@ -23,13 +23,35 @@ class transform_gui(object):
     def __init__(self):
         #   VARIABLES
         
+        self.image = self.open_image()
+        
         #   CREATE WIDGETS
+        
         #   Every GUI must have one instance of QApplication(), inside the brackets[] would be parameters passed to the application
         self.app = qt.QtWidgets.QApplication([])
         self.app.setStyle('Fusion')
 
         self.label = qt.QtWidgets.QLabel("Whaddup playa")
         self.label.show()
+        
+    #   GUI FUNCTIONS
+    def open_image(self):
+        
+        #   Not having Tk().withdraw() does not make the code execute properly, not completely sure why, but it is necessary
+        Tk().withdraw()
+        #   Opens up file explorer to select the file
+        filePath = askopenfilename()
+        fileName = os.path.basename(filePath)
+        print("File Name:",fileName)
+
+        #   Opening fits file. Returns Header Data Unit(HDU) List (hdul: header and data array/table)
+        hdul = fits.open(filePath)
+
+        #   Primary HDU
+        primHDU = hdul[0]
+        imageArray = primHDU.data[:,:]
+        
+        return imageArray
 
 def main():
     
