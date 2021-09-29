958ae0654f278a4771843e35dba007a8ff02ca80
Earthman12 <dteran08@gmail.com>
2021-09-29 09:51:11 -0700

    Update fourier_transform_v5.py

--- fourier_transform_v5.py
+++ fourier_transform_v5.py
@@ -13,6 +13,7 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas, NavigationToolbar2QT as NavigationToolbar
 import numpy as np
 import scipy.fft
+from scipy.interpolate import make_interp_spline
 from astropy.io import fits
 from astroscrappy import detect_cosmics
 from PyQt5 import QtWidgets
