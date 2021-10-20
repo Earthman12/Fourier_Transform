49b675143022851da6c3f8be8728d6f61d25b0ef
Earthman12 <dteran08@gmail.com>
2021-10-20 09:44:57 -0700

    Making it organized

--- shs_kitt_peakpyth4b/shs_kitt_peakpyth4b_dt_fix.py
+++ shs_kitt_peakpyth4b/shs_kitt_peakpyth4b_dt_fix.py
@@ -677,22 +677,26 @@ class Main(QMainWindow):
             ax.imshow((numpy.log10(pwr_sp)),cmap = cm.Greys_r,vmin=minv, vmax=maxv)
             ax.set_frame_on(False)
             ax.set_xticks([]); ax.set_yticks([])
-            plt.show()
+            
+            #plt.show()
        #        ax.imshow(data3,cmap = cm.Greys_r,vmin=vmin_in, vmax=vmax_in)
             #plt.savefig('th_ar_pwr_test.svg', bbox_inches='tight',format='svg', dpi=1000, transparent=True,pad_inches=0)              
        ###END PLOT POWER     
             
-            bb=ax.imshow((numpy.log10(pwr_sp)),cmap = cm.Greys_r,vmin=minv, vmax=maxv/.8, picker=True)
+            #bb=ax.imshow((numpy.log10(pwr_sp)),cmap = cm.Greys_r,vmin=minv, vmax=maxv/.8, picker=True)
             ax.invert_yaxis()
+            
             ax2 = fig2.add_subplot(222)
 
             print(("The Min is", minv))
-            
             print(("The max is",maxv))
+            
+            #   Transform axis
             axcolor = 'lightgoldenrodyellow'
             axfreq = plt.axes([0.1, 0.01, 0.8, 0.04], facecolor=axcolor)
             sfreq = Slider(axfreq, 'y_max', minv/.9, maxv/.9, valinit=maxv) 
 
+            #   Unused axis'
             ax3 = fig2.add_subplot(223)
             ax4 = fig2.add_subplot(224)
             ax3.set_title('Selected Rows')
